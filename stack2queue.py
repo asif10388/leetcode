bf7d81083e209481b9c6261910c3d7dd46e247b9
asif10388 <asif.khan10388@gmail.com>
2023-02-24 19:27:59 +0600

    Implement Queue using Stacks

---
+++ stack2queue.py
@@ -0,0 +1,39 @@
+class Queue:
+    def __init__(self):
+        self.stack1 = []        
+        self.stack2 = []
+
+    def peek(self):
+        if(self.stack2 != []):
+            return self.stack2[-1]
+        elif(self.stack1 != []):
+            return self.stack1[0]
+        else:
+            return False
+
+    def enqueue(self, item):
+        return self.stack1.append(item)
+    
+    def dequeue(self):
+        if(self.stack2 == []):
+            for i in range(len(self.stack1)):
+                self.stack2.append(self.stack1.pop())
+            return self.stack2.pop()
+        else:
+            return self.stack2.pop()
+    
+    def isEmpty(self):
+        return self.stack1 + self.stack2 == []
+
+    def printQueue(self):
+        return self.stack1 + self.stack2
+
+queue = Queue()
+
+queue.enqueue(1); # queue is: [1]
+queue.enqueue(2); # queue is: [1, 2] (leftmost is front of the queue)
+print(queue.peek()); # return 1
+print(queue.dequeue()); # return 1, queue is [2]
+print(queue.isEmpty()); # return false
+
+print(queue.printQueue())
