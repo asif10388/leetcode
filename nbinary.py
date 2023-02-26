a5de58a78e42888b9ff02fe898bde313d50cba34
asif10388 <asif.khan10388@gmail.com>
2023-02-26 21:13:27 +0600

    number to binary

---
+++ nbinary.py
@@ -0,0 +1,42 @@
+class Queue:
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def peek(self):
+        if(self.stack2 != []):
+            return self.stack2[-1]
+        elif(self.stack1 != []):
+            return self.stack1[0]
+        else:
+            return False
+
+    def enqueue(self, item):
+        return self.stack1.append(item)
+
+    def dequeue(self):
+        if(self.stack2 == []):
+            for i in range(len(self.stack1)):
+                self.stack2.append(self.stack1.pop())
+            return self.stack2.pop()
+        else:
+            return self.stack2.pop()
+
+    def isEmpty(self):
+        return self.stack1 + self.stack2 == []
+
+    def printQueue(self):
+        queue = self.stack1 + self.stack2
+        return queue
+
+
+queue = Queue()
+
+
+def nToBinary(n, queue):
+    for i in range(n + 1):
+        queue.enqueue(bin(i).replace("b", ""))
+    return "\n\n".join([*queue.printQueue()])
+
+
+print(nToBinary(1, queue))
